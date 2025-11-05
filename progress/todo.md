4c167a15ab67535beb672feeb6c363b7830d059f
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2025-11-05 23:57:22 +0200

    add AuthModule and update app.module.ts to include it; create todo.md for project planning

---
+++ progress/todo.md
@@ -0,0 +1,7 @@
+## Thrive working plan
+
+## Goal: Build a thriving community around Thrive
+
+## Tasks
+
+The system will include multable parts that help you mange your hapits / goals / tasks / projects / day to day notifications.
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 
+// modules imports
+import { AuthModule } from './modules/auth/auth.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/fanous_app', {
@@ -27,6 +29,7 @@ import { Connection } from 'mongoose';
         return connection;
       },
     }),
+    AuthModule,
   ],
 })
 export class AppModule {}
